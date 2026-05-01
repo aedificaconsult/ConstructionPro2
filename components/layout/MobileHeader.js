0c827f25319dc3632e80f2be03f3b8a37a6f31db
Abey Lulseged <aedificaconsult@gmail.com>
2026-05-01 17:59:13 +0300

    Create MobileHeader component for layout

---
+++ components/layout/MobileHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MobileHeader = () => {
+    return (
+        <header className="mobile-header">
+            <h1>Construction Pro</h1>
+            {/* Add navigation or other components here */}
+        </header>
+    );
+};
+
+export default MobileHeader;
